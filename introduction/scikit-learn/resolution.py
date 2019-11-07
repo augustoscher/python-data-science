0dffc206148db1b47e4b323277445d437716c983
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-07 01:13:00 -0300

    resolution knn

--- introduction/scikit-learn/resolution.py
+++ introduction/scikit-learn/resolution.py
@@ -11,36 +11,46 @@ from sklearn.neighbors import KNeighborsClassifier
 from scipy.stats import mode
 
 
-def classify(k, train, X_test):
+# def classify(k, train, X_test):
 
-  # X_train contains the training features
-  # X_test contains the testing features
-  # y_train contains the training label
-  # y_test contains the testing labels
-  (X_train, y_train) = train
+#   # X_train contains the training features
+#   # X_test contains the testing features
+#   # y_train contains the training label
+#   # y_test contains the testing labels
+#   (X_train, y_train) = train
 
-  classifier = KNeighborsClassifier(n_neighbors=k)
-  classifier.fit(X_train, y_train)
-  y_pred = classifier.predict(X_test)
+#   classifier = KNeighborsClassifier(n_neighbors=k)
+#   classifier.fit(X_train, y_train)
+#   y_pred = classifier.predict(X_test)
 
-  for i in range(len(X_test)):
-        # Busca linha por linha da matriz
-        x = X_test[i, :]
+#   for i in range(len(X_test)):
+#         # Busca linha por linha da matriz
+#         x = X_test[i, :]
 
-        # ** 2 é ao quadrado
-        # distancia = np.sum((p1 - p2) ** 2)
-        # print(distancia)
+#         # ** 2 é ao quadrado
+#         # distancia = np.sum((p1 - p2) ** 2)
+#         # print(distancia)
 
-        # calcula distância euclidiana
-        diff_quadrado = (X_train - x) ** 2
-        distancia = diff_quadrado.sum(axis=1)
-        # print(distancia)
+#         # calcula distância euclidiana
+#         diff_quadrado = (X_train - x) ** 2
+#         distancia = diff_quadrado.sum(axis=1)
+#         # print(distancia)
 
-        # ordena da menor para a maior distância e pega as primeiras "k" distâncias
-        min_k = distancia.argsort()[:k]
-        # pega o valor mais frequente (moda)
-        label = mode(y_train[min_k]).mode[0]
-        yield label
+#         # ordena da menor para a maior distância e pega as primeiras "k" distâncias
+#         min_k = distancia.argsort()[:k]
+#         # pega o valor mais frequente (moda)
+#         label = mode(y_train[min_k]).mode[0]
+#         yield label
+
+
+
+def get_short_distance_indixes(k, distances):
+  indexes = np.array()
+
+  # for i in range(len(distances)):
+  
+  return indexes
+
 
 # X is iris.data
 # y is iris.target
@@ -50,5 +60,16 @@ def classify(k, train, X_test):
 # Usa 80% dos dados para treinar o modelo
 # Os 20% restantes são usados para testar o modelo
 (X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2, random_state=1)
-classify(k=5, train=(X_train, y_train), X_test=X_test)
+# classify(k=5, train=(X_train, y_train), X_test=X_test)
+
+distances = np.array()
+for i in range(len(X_test)):
+  r = X_test[i,:] - X_train[i,:]
+  # Buscar as distancias euclidiana
+  distances.append(np.square(r).sum())
+
+# Com base no array de distancias, 
+# descobrir o  indice dos dos k menores distancias.
+
+# indexes = get_short_distance_indixes(k=3, distances)
 
