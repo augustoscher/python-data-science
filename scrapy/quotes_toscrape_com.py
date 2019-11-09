94b0c65d14cc1e5b317653c834f8f459e13f30fd
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-09 15:04:57 -0300

    scrapy generating csv and json

--- scrapy/quotes_toscrape_com.py
+++ scrapy/quotes_toscrape_com.py
@@ -1,13 +1,19 @@
-# Para executar: scrapy runspider quotes_toscrape_com.py
+# Para executar:     scrapy runspider quotes_toscrape_com.py
+# Salvar em arquivo: scrapy runspider quotes_toscrape_com.py -o quotes.csv
+# Salvar em arquivo: scrapy runspider quotes_toscrape_com.py -o quotes.json
 import scrapy
 
 
 class QuotesToscrapeComSpider(scrapy.Spider):
-    name = 'quotes.toscrape.com'
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+  name = 'quotes.toscrape.com'
+  allowed_domains = ['quotes.toscrape.com']
+  start_urls = ['http://quotes.toscrape.com/']
 
-    def parse(self, response):
-        for quote in response.css('.quote'):
-            text = quote.css('span.text::text').get()
-            yield {'text': text}
+  def parse(self, response):
+    for quote in response.css('.quote'):
+      text = quote.css('span.text::text').get()
+      autor = quote.css('.author::text').get()
+      yield {
+        'text': text,
+        'author': autor
+      }
