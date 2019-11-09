6786a2243312c00c79ab15e2a2ef34b1f9bd37e4
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-09 09:31:30 -0300

    aula 2

--- introduction/scikit-learn/resolution.py
+++ introduction/scikit-learn/resolution.py
@@ -1,7 +1,6 @@
 # Reference: 
 # https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
-# Aprendizado supervisionado - Regressão Linear
-# Tem uma entrada esperada como expected
+# Aprendizado supervisionado - KNN
 
 import numpy as np
 from sklearn.datasets import load_iris
@@ -42,34 +41,36 @@ from scipy.stats import mode
 #         label = mode(y_train[min_k]).mode[0]
 #         yield label
 
-
-
-def get_short_distance_indixes(k, distances):
-  indexes = np.array()
-
-  # for i in range(len(distances)):
-  
-  return indexes
-
+# ================================================================================================
 
 # X is iris.data
-# y is iris.target
+# y is iris.targedistanciat
 # X contains the features and y contains the labels
 (X, y) = load_iris(return_X_y=True)
 
 # Usa 80% dos dados para treinar o modelo
 # Os 20% restantes são usados para testar o modelo
-(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2, random_state=1)
+# x_train e x_test é oque eu conheço.
+# y_train e y_test é o que quero descobrir
+(X_train, X_test, y_train, y_test) = train_test_split(X, y, test_size=0.2, random_state=42)
 # classify(k=5, train=(X_train, y_train), X_test=X_test)
 
-distances = np.array()
+k = 5
+predicoes = []
 for i in range(len(X_test)):
-  r = X_test[i,:] - X_train[i,:]
-  # Buscar as distancias euclidiana
-  distances.append(np.square(r).sum())
+  # pega uma linha e diminui de uma matriz
+  # tira a raiz quadrada
+  distances = np.square(X_test[i, :] - X_train)
+  # soma os valores da matriz, coluna por coluna
+  distances = np.sum(distances, axis=1)
 
-# Com base no array de distancias, 
-# descobrir o  indice dos dos k menores distancias.
+  # Retorna os indices ordenados
+  indices = np.argsort(distances)
+  top_k = indices[:k]
 
-#   indexes = get_short_distance_indixes(k=3, distances)
+  labels = y_train[top_k]
+  moda = mode(labels)
+  predicoes.append(moda.mode[0])
+
+print(f'Predições: ', predicoes)
 
