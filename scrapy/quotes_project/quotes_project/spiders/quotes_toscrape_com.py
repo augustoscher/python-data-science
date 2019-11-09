ec4954ad892bbac2e3f5cfe3ae2c28b918a51b01
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-09 15:54:23 -0300

    scrapy generating project

---
+++ scrapy/quotes_project/quotes_project/spiders/quotes_toscrape_com.py
@@ -0,0 +1,24 @@
+# Para executar:     scrapy runspider spiders/quotes_toscrape_com.py
+# Salvar em arquivo: scrapy runspider spiders/quotes_toscrape_com.py -o quotes.csv
+# Salvar em arquivo: scrapy runspider spiders/quotes_toscrape_com.py -o quotes.json
+import scrapy
+
+
+class QuotesToscrapeComSpider(scrapy.Spider):
+  name = 'quotes.toscrape.com'
+  allowed_domains = ['quotes.toscrape.com']
+  start_urls = ['http://quotes.toscrape.com/']
+
+  def parse(self, response):
+    for quote in response.css('.quote'):
+      text = quote.css('span.text::text').get()
+      autor = quote.css('.author::text').get()
+      yield {
+        'text': text,
+        'author': autor
+      }
+    
+    # Paginação
+    url = response.css('.pager .next a::attr(href)').get()
+    if url is not None:
+      yield response.follow(url)
