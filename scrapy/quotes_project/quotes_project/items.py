66ab0f94327bd70d91584eaa3b734480f1285d47
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-09 16:06:09 -0300

    scrapy generating project

--- scrapy/quotes_project/quotes_project/items.py
+++ scrapy/quotes_project/quotes_project/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class QuotesProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuotesItem(scrapy.Item):
+    author = scrapy.Field()
+    text = scrapy.Field()
+    scrape_date = scrapy.Field()
+    url = scrapy.Field()
+    rank = scrapy.Field()
--- scrapy/quotes_project/quotes_project/spiders/quotes_toscrape_com.py
+++ scrapy/quotes_project/quotes_project/spiders/quotes_toscrape_com.py
@@ -3,6 +3,7 @@
 # Salvar em arquivo: scrapy runspider spiders/quotes_toscrape_com.py -o quotes.json
 import scrapy
 
+from quotes_project.items import QuotesItem
 
 class QuotesToscrapeComSpider(scrapy.Spider):
   name = 'quotes.toscrape.com'
@@ -13,11 +14,13 @@ class QuotesToscrapeComSpider(scrapy.Spider):
     for quote in response.css('.quote'):
       text = quote.css('span.text::text').get()
       autor = quote.css('.author::text').get()
-      yield {
-        'text': text,
-        'author': autor
-      }
-    
+      yield QuotesItem (
+        text=text,
+        author=autor,
+        url=response.url,
+        rank=1
+      )
+        
     # Paginação
     url = response.css('.pager .next a::attr(href)').get()
     if url is not None:
