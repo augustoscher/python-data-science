17ee0bd6607197b3503e15c569d3f4a000a7d2c6
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-11-19 11:16:42 -0300

    adding books.toscrape project

---
+++ scrapy/books_project/books_project/spiders/books_toscrape_com.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BooksToscrapeComSpider(scrapy.Spider):
+    name = 'books.toscrape.com'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        pass
